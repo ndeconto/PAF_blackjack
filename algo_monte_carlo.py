2a2caba20775fd8db250a0d08b192dbfec00587f
lcombaldieu <louis-combaldieu@orange.fr>
2017-06-16 16:02:15 +0200

    rajouté les états liés à la carte de l'adversaire

--- algo_monte_carlo.py
+++ algo_monte_carlo.py
@@ -4,20 +4,21 @@ from random import *;
 
 actions = {"draw":0, "fold":1};        #Set d'actions disponibles
 states = range(12);           #Set de states disponibles {<12, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, >21}
+enemystate = {0,1,2,3,4,5,6,7,8,9,10} # 0 = tête
 alpha = 0.95;                 #Taux d'évaporation
 epsilon = 0.05;               #seuil de valeur minimal
-mypolicy = [[1.0]*len(actions) for i in range(len(states))];    #valeurs initiales de chaques coups dans chaque situatio 
+mypolicy = [[[1.0]*len(actions) for i in range(len(states))] for j in range(len(enemystate))];    #valeurs initiales de chaques coups dans chaque situatio 
 
 
 		
 def getRealValues():          #renvoie le poids de chaque décision normalisé
 	return(policy*(1-alpha));
 	
-def makeDecision(state):      #renvoie une décision étant donné un état
+def makeDecision(state,enemystate):      #renvoie une décision étant donné un état
 	if state<12 : ind = 0
 	elif state>21 : ind = 11
 	else: ind = state-11
-	coeff = mypolicy[ind][:];    #liste des poids des décisions dans l'état donné
+	coeff = mypolicy[enemystate][ind][:];    #liste des poids des décisions dans l'état donné
 	summ = 0;                 #summ est un coefficient de normalisation
 	for i in coeff:
 		summ += i;
@@ -28,4 +29,15 @@ def makeDecision(state):      #renvoie une décision étant donné un état
 		if x <= decis : return(i);   #prise de décision aléatoire
 	return(len(coeff)-1);
 	
-
+def makeDecision2(state,enemystate):
+	if state<12 : ind = 0
+	elif state>21 : ind = 11
+	else: ind = state-11
+	coeff = mypolicy[enemystate][ind][:];
+	x = random();
+	if x<epsilon:            #prise de décision non optimal avec proba epsilon
+		return(randint(len(coeff)));
+	s=0;
+	for i in range(len(coeff)):
+		if coeff[i]>coeff[s]: s=i;
+	return(s)
