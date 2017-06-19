d22572ded2e9192cd42eece9b4d5ea32c586c392
lcombaldieu <louis-combaldieu@orange.fr>
2017-06-19 14:32:43 +0200

    enlevé ligne inutile

--- cartes.py
+++ cartes.py
@@ -54,7 +54,6 @@ class Carte:
         if 1 <= self.hauteur <= 13: return min(self.hauteur,10)
 
         if self.hauteur == AS: return [1, 11]
-        if self.hauteur == ASONZE: return(11)
         return 10
 
 
