576f3cbb8df6f910303128bdffa48bfd773d6596
Romain Deffayet <romain.deffayet@telecom-paristech.fr>
2017-06-19 15:02:08 +0200

    Affichage de la matrice des actions effectuées

--- boucle_apprentissage.py
+++ boucle_apprentissage.py
@@ -6,6 +6,10 @@ Created on Sun Jun 18 18:23:29 2017
 @author: majdagoumi
 """
 from idee_automatisation_jeux import *
+from algo_monte_carlo import *
+
+def f(values):
+	return values.index(max(values))
 
 def apprentissage(n):
     victoire = 0
@@ -15,4 +19,8 @@ def apprentissage(n):
     print("Stats victoires : ",stat_gain)
     print("Nb etat joué et nb etats ganés : ", nombre_etats_joue,nombre_etats_gagnes)
     print("Pourcentage de victoire : ",100*nombre_etats_gagnes[0]/nombre_etats_joue[0])
+    result = [[f(values) for values in i]for i in mypolicy]
+    print("     ↑ low enemy card   | high enemy card ↓")
+    print("     <- low value hand  |  high value hand ->")
+    print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in result]))
     
