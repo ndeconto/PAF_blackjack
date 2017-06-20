40041e43ca911a2e946bd5c14bdc887b2659ef18
Majd Agoumi <majdagoumi22@gmail.com>
2017-06-20 15:32:33 +0200

    modification boucle apprentissage

--- boucle_apprentissage.py
+++ boucle_apprentissage.py
@@ -25,6 +25,20 @@ def apprentissage(n):
     print("     <- low value hand  |  high value hand ->")
     print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in result]))
 
+def apprentissage2(n,bet):
+    victoire = 0
+    for k in range(n):
+        victoire = manche2(bet)
+    print("Stats victoires : ",stat_gain)
+    print("Nb etat joué et nb etats ganés : ", nombre_etats_joue,nombre_etats_gagnes)
+    print("Pourcentage de victoire : ",100*nombre_etats_gagnes[0]/nombre_etats_joue[0])
+
+    result = [[f(values) for values in i]for i in mypolicy]
+    print("     ↑ low enemy card   | high enemy card ↓")
+    print("     <- low value hand  |  high value hand ->")
+    print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in result]))
+
+
 def save_mypolicy(policy):
     file_handler = open("mypolicy", "w")
     pickle.dump(policy,file_handler)
