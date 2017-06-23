198f75f8a9e13a5263b3aa34acb97c69de5158ff
Romain Deffayet <romain.deffayet@telecom-paristech.fr>
2017-06-23 15:20:53 +0200

    Affichage des matrices de décision terminé !

--- boucle_apprentissage.py
+++ boucle_apprentissage.py
@@ -5,13 +5,13 @@ Created on Sun Jun 18 18:23:29 2017
 
 @author: majdagoumi
 """
-from idee_automatisation_jeux import *
 from algo_MC_2 import *
 from prise_2_decision import *
 import numpy as np
-import csv
-import collections
 from openpyxl import Workbook
+from openpyxl.styles import Color, PatternFill, Font, Border, Side, Alignment
+from openpyxl.styles.differential import DifferentialStyle
+from openpyxl.formatting.rule import ColorScaleRule, CellIsRule, FormulaRule
 from load_object import *
 
 
@@ -58,17 +58,157 @@ def save_mypolicy(p1,p2,p3): #Le fichier sauvegarde est un vecteur comportant le
 
 def save_to_xlsx():
     wb = Workbook()
+
     
     # grab the active worksheet
     ws = wb.active
     
-    ws['A1']="Simple :"
-    for row in getPolicy()[0] :
-        ws.append(row)
     
+                        
+    
+    ws['A1']="Simple :"
+    ws['A2']=""
+    ws.append(["","< 9"]+list(range(9,22))+["> 21"])
+    
+    policy = getPolicy()[:]
+    i=1
+    for row in policy[0] :
+        for k in range(len(row)):
+            row[k] = row[k].index(max(row[k]))
+        ws.append([i]+row)
+        i+=1
+    
+    ws['A14'] = ""
+    ws['A15'] = "As :"
+    ws['A16'] = ""
+    ws.append([""]+["A,"+str(k) for k in range (2,11)])
+    
+    i=1
+    for row in policy[1] :
+        for k in range(len(row)):
+            row[k] = row[k].index(max(row[k]))
+        ws.append([i]+row)
+        i+=1
+    
+    ws['A28'] = ""
+    ws['A29'] = "Paires :"
+    ws['A30'] = ""
+    ws.append([""]+["A,A"]+[str(k)+","+str(k) for k in range (2,11)])
+    
+    i=1
+    for row in policy[2] :
+        for k in range(len(row)):
+            row[k] = row[k].index(max(row[k]))
+        ws.append([i]+row)
+        i+=1
+    
+    ws.conditional_formatting.add('B4:P13',
+    ColorScaleRule(start_type='percentile', start_value=10, start_color='FF6666',
+                        mid_type='percentile', mid_value=50, mid_color='FFC966',
+                        end_type='percentile', end_value=90, end_color='6DC066')
+                  )
+    
+    ws.conditional_formatting.add('B18:J27',
+    ColorScaleRule(start_type='percentile', start_value=10, start_color='FF6666',
+                        mid_type='percentile', mid_value=50, mid_color='FFC966',
+                        end_type='percentile', end_value=90, end_color='6DC066')
+                  )
+    
+    redFill = PatternFill(start_color='FF6666',
+                   end_color='FF6666',
+                   fill_type='solid')
+    
+    orangeFill = PatternFill(start_color='FFC966',
+                        end_color='FFC966',
+                        fill_type='solid')
+    
+    greenFill = PatternFill(start_color='6DC066',
+                        end_color='6DC066',
+                        fill_type='solid')
+    
+    alignment=Alignment(horizontal='center',
+                        vertical='center',
+                        text_rotation=0,
+                        wrap_text=False,
+                        shrink_to_fit=False,
+                        indent=0)
+    
+    
+    rows = ws['A3:P13']
+    side = Side(border_style='thin', color="FF000000")
+
+    rows = list(rows)  # we convert iterator to list for simplicity, but it's not memory efficient solution
+    max_y = len(rows) - 1  # index of the last row
+    for pos_y, cells in enumerate(rows):
+        max_x = len(cells) - 1  # index of the last cell
+        for pos_x, cell in enumerate(cells):
+            border = Border(
+                left=cell.border.left,
+                right=cell.border.right,
+                top=cell.border.top,
+                bottom=cell.border.bottom
+            )
+            border.left = side
+            border.right = side
+            border.top = side
+            border.bottom = side
+
+            cell.border = border
+            cell.alignment=alignment
+    
+    rows = ws['A17:J27']
+    side = Side(border_style='thin', color="FF000000")
+
+    rows = list(rows)  # we convert iterator to list for simplicity, but it's not memory efficient solution
+    max_y = len(rows) - 1  # index of the last row
+    for pos_y, cells in enumerate(rows):
+        max_x = len(cells) - 1  # index of the last cell
+        for pos_x, cell in enumerate(cells):
+            border = Border(
+                left=cell.border.left,
+                right=cell.border.right,
+                top=cell.border.top,
+                bottom=cell.border.bottom
+            )
+            border.left = side
+            border.right = side
+            border.top = side
+            border.bottom = side
+
+            cell.border = border
+            cell.alignment=alignment
+    
+    rows = ws['A31:K41']
+    side = Side(border_style='thin', color="FF000000")
+
+    rows = list(rows)  # we convert iterator to list for simplicity, but it's not memory efficient solution
+    max_y = len(rows) - 1  # index of the last row
+    for pos_y, cells in enumerate(rows):
+        max_x = len(cells) - 1  # index of the last cell
+        for pos_x, cell in enumerate(cells):
+            border = Border(
+                left=cell.border.left,
+                right=cell.border.right,
+                top=cell.border.top,
+                bottom=cell.border.bottom
+            )
+            border.left = side
+            border.right = side
+            border.top = side
+            border.bottom = side
+            if (cell.value==0 and pos_x!=0) :
+                cell.fill=redFill
+            if (cell.value==1 and pos_x!=0) :
+                cell.fill=orangeFill
+            if (cell.value==2 and pos_x!=0) :
+                cell.fill=greenFill
+            if (cell.value==3 and pos_x!=0) : 
+                cell.style='Accent4'
+            cell.border = border
+            cell.alignment=alignment
     
     # Save the file
-    wb.save("policy_simple.xlsx")
+    wb.save("policy.xlsx")
 
 
 
